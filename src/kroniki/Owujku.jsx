a409395b8ae28fc92094eb28a7112bf07f7b6223
Kolmipilot <mati.ki@onet.pl>
2024-10-11 16:26:56 +0200

    komentarz do niedziałajacch kornik

--- src/kroniki/Owujku.jsx
+++ src/kroniki/Owujku.jsx
@@ -1,41 +1,35 @@
-const Owujku = "Był sobie pewien wujek, co przed pracą lubił wypić,
-Gdy wyruszał w długą drogę, pragnienie zaczęło go gnębić.
-Bez długiego namysłu, do monopolowego wstąpił,
-Wsiadł na rower, w drodze do wsi, pragnienie w nim wzrosło.
+/*const Owujku = 'Był sobie pewien wujek, co przed pracą lubił wypić,  
+Gdy wyruszał w długą drogę, pragnienie zaczęło go gnębić.  
+Bez długiego namysłu, do monopolowego wstąpił,  
+Wsiadł na rower, w drodze do wsi, pragnienie w nim wzrosło.  
+W tej wsi, pod jedyny sklep, z trzęsącymi rękoma dotarł,  
+Rower do płotu przypiął, choć ledwo co się trzymał.  
+Wszedł do sklepu, gdzie trunek wymarzony czekał,  
+Szybko go wypił, na rower wskoczył, w dal pojechał.  
+Jednak po chwili, ziemia pod nim zaczęła uciekać,  
+Choć walczył dzielnie, w końcu musiał się poddać.  
+Do rowu wpadł, zmęczony i pokonany,  
+Tak skończył się dzień, który miał być zwyczajny.  
+W rowie leżał, myśli w głowie kłębiły się gęsto,  
+Czy warto było pić, czy to tylko złudne szczęście?  
+Wspominał chwile, gdy życie było prostsze,  
+Gdy nie musiał walczyć z nałogiem, co go niszczył.  
+Wstał z rowu, otrzepał się z kurzu,  
+Postanowił zmienić życie, choćby w wielkim trudzie.  
+Rower podniósł, choć ręce wciąż drżały,  
+Ruszył w drogę, by odnaleźć spokój upragniony.  
+Droga była długa, pełna wyzwań i przeszkód,  
+Lecz wujek wiedział, że musi iść naprzód.  
+Każdy krok, choć trudny, przybliżał go do celu,  
+By w końcu znaleźć spokój, z dala od gorzały.  
+Lecz w sercu tliła się wciąż niepewność, jak cień,  
+Czy uda się mu wstać, czy znowu znów sięgnie?  
+Bo choć wujek walczył, los miał dla niego plany,  
+Znów się potknął, wpadł w wir – gorzki los jest bezlitosny.  
+Smutne zakończenie, które znów go pokonało,  
+W rowie leżał, sam w ciemności, znów smutny i mały.  
+Czy życie znów mu da szansę, czy zamknie drzwi?  
+Znowu z trwogą w sercu, znów wódką swą skryty.
+'
 
-W tej wsi, pod jedyny sklep, z trzęsącymi rękoma dotarł,
-Rower do płotu przypiął, choć ledwo co się trzymał.
-Wszedł do sklepu, gdzie trunek wymarzony czekał,
-Szybko go wypił, na rower wskoczył, w dal pojechał.
-
-Jednak po chwili, ziemia pod nim zaczęła uciekać,
-Choć walczył dzielnie, w końcu musiał się poddać.
-Do rowu wpadł, zmęczony i pokonany,
-Tak skończył się dzień, który miał być zwyczajny.
-
-W rowie leżał, myśli w głowie kłębiły się gęsto,
-Czy warto było pić, czy to tylko złudne szczęście?
-Wspominał chwile, gdy życie było prostsze,
-Gdy nie musiał walczyć z nałogiem, co go niszczył.
-
-Wstał z rowu, otrzepał się z kurzu,
-Postanowił zmienić życie, choćby w wielkim trudzie.
-Rower podniósł, choć ręce wciąż drżały,
-Ruszył w drogę, by odnaleźć spokój upragniony.
-
-Droga była długa, pełna wyzwań i przeszkód,
-Lecz wujek wiedział, że musi iść naprzód.
-Każdy krok, choć trudny, przybliżał go do celu,
-By w końcu znaleźć spokój, z dala od gorzały.
-
-Lecz w sercu tliła się wciąż niepewność, jak cień,
-Czy uda się mu wstać, czy znowu znów sięgnie?
-Bo choć wujek walczył, los miał dla niego plany,
-Znów się potknął, wpadł w wir – gorzki los jest bezlitosny.
-
-Smutne zakończenie, które znów go pokonało,
-W rowie leżał, sam w ciemności, znów smutny i mały.
-Czy życie znów mu da szansę, czy zamknie drzwi?
-Znowu z trwogą w sercu, znów wódką swą skryty."
-
-export default Owujku ;
+export default Owujku ;*/
