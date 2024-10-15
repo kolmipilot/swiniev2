b9b9df5b2ce747f5c480a770b59b18890aac22da
Kolmipilot <mati.ki@onet.pl>
2024-10-15 17:01:13 +0200

    poprawienie responsywnosci modala

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ isOpen, onClose, title, content }) => {
       <div className="bg-white rounded-lg shadow-lg p-6 max-h-screen max-w-screen-md w-full h-auto rounded-3xl object-cover border-gray-200 border-2">
         <h2 className="text-xl font-bold bg-white">{title}</h2>
         <div className="overflow-y-auto max-h-[70vh] my-4"> {/* Zawijamy wiersz w przewijalny kontener */}
-          <pre className=" bg-slate-100 text-center px-[5vw] text-lg font-mono font-normal">{content}</pre> {/* Pre with wrapping */}
+          <p className=" bg-slate-100 text-center px-[5vw] text-lg font-mono font-normal whitespace-break-spaces md:whitespace-pre">{content}</p> {/* Pre with wrapping */}
         </div>
         <button className="mt-4 bg-blue-500 text-white rounded px-4 py-2" onClick={onClose}>
           Zamknij
